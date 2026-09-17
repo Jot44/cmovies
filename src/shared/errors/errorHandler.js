1569e1e2769c44f20fc5370d7cc6aaf0668e9f61
João Victor Franco <j-joaovictor2010@hotmail.com>
2026-09-17 19:42:28 -0300

    feat: adiciona tratamento de erros customizado

---
+++ src/shared/errors/errorHandler.js
@@ -0,0 +1,8 @@
+function errorHandler(err, req, res, next) {
+  const statusCode = err.statusCode || 500;
+  const message = err.statusCode ? err.message : "Erro interno do servidor";
+
+  res.status(statusCode).json({ error: message });
+}
+
+module.exports = errorHandler;
